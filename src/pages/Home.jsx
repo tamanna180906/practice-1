0ae0dbf8661c143a85fa1dccd55929d86551f92a
tamanna180906 <myeuphoria330@gmail.com>
2025-08-18 02:36:34 +0600

    add a

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import Icone from '../components/Icone'
+import Footer from '../components/Footer'
+import Sale from '../components/Sale'
+import Offers from '../components/Offers'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <Banner/>
+    <Icone/>
+    <Sale/>
+    <Offers/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
